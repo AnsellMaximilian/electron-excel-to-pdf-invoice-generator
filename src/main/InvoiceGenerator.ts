b4f80a94e40ceb7247a2310aebaf9836b444d458
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-05 15:18:32 +0700

    start combine

--- src/main/InvoiceGenerator.ts
+++ src/main/InvoiceGenerator.ts
@@ -157,6 +157,7 @@ class InvoiceGenerator {
   static generateSubtotal(
     doc: PDFKit.PDFDocument,
     subtotal: number,
+    label: string,
     priceX: number,
     subtotalColumnWidth: number
   ) {
@@ -166,7 +167,7 @@ class InvoiceGenerator {
       .moveDown()
       .font('Helvetica-Bold')
       .fontSize(15)
-      .text('SUBTOTAL:', priceX, undefined, {
+      .text(label, priceX, undefined, {
         width: subtotalColumnWidth,
         align: 'left',
       })
@@ -180,6 +181,7 @@ class InvoiceGenerator {
   static generateGrandTotal(
     doc: PDFKit.PDFDocument,
     grandTotal: number,
+    label: string,
     priceX: number,
     subtotalColumnWidth: number
   ) {
@@ -189,7 +191,7 @@ class InvoiceGenerator {
       .moveDown()
       .fontSize(17.5)
       .font('Helvetica-Bold')
-      .text('Grand Total:', priceX, undefined, {
+      .text(label, priceX, undefined, {
         align: 'left',
         width: subtotalColumnWidth,
       })
@@ -198,8 +200,6 @@ class InvoiceGenerator {
       .font('Helvetica')
       .fontSize(10);
   }
-
-  static generateCombinedInvoices(...invoices: InvoiceData[]) {}
 
   static generate(invoice: Invoice): PDFKit.PDFDocument | null {
     const output = new PDFGenerator();
@@ -262,6 +262,7 @@ class InvoiceGenerator {
     InvoiceGenerator.generateSubtotal(
       output,
       subtotal,
+      'Subtotal:',
       priceX,
       subtotalColumnWidth
     );
@@ -311,12 +312,118 @@ class InvoiceGenerator {
     InvoiceGenerator.generateGrandTotal(
       output,
       grandTotal,
+      'Grand Total:',
       priceX,
       subtotalColumnWidth
     );
 
     output.end();
     return null;
+  }
+
+  static generateCombinedInvoices(...invoices: InvoiceData[]) {
+    // const output = new PDFGenerator();
+    // const { width, margins } = output.page;
+    // const widthAfterMargins = width - margins.left - margins.right;
+    // const startOfPage = 0 + margins.left;
+    // // Pipe into pdf file
+    // output.pipe(
+    //   fs.createWriteStream(
+    //     path.join(
+    //       app.getPath('home'),
+    //       'Rumah Sehat Archive',
+    //       'Invoices',
+    //       `${invoice.invoiceData.name} ${invoice.invoiceData.date}.pdf`
+    //     )
+    //   )
+    // );
+    // // Header
+    // const headerHalfWidth = width / 2;
+    // InvoiceGenerator.generateHeader(
+    //   output,
+    //   headerHalfWidth,
+    //   invoice.invoiceData.name,
+    //   invoice.invoiceData.date
+    // );
+    // output.moveDown().fontSize(10);
+    // // Table
+    // const itemColumnWidth = widthAfterMargins * 0.4;
+    // const priceColumnWidth = widthAfterMargins * 0.2;
+    // const qtyColumnWidth = widthAfterMargins * 0.2;
+    // const itemX = startOfPage;
+    // const priceX = itemX + itemColumnWidth;
+    // const qtyX = priceX + priceColumnWidth;
+    // const itemTotalX = qtyX + qtyColumnWidth;
+    // InvoiceGenerator.generateTable(
+    //   output,
+    //   invoice,
+    //   startOfPage,
+    //   itemColumnWidth,
+    //   priceColumnWidth,
+    //   qtyColumnWidth,
+    //   itemX,
+    //   priceX,
+    //   qtyX,
+    //   itemTotalX
+    // );
+    // const amountX = itemTotalX;
+    // // Subtotal
+    // const subtotalColumnWidth = priceColumnWidth + qtyColumnWidth;
+    // const subtotal = invoice.getSubtotal();
+    // InvoiceGenerator.generateSubtotal(
+    //   output,
+    //   subtotal,
+    //   'Subtotal:',
+    //   priceX,
+    //   subtotalColumnWidth
+    // );
+    // // Delivery fees
+    // const hasDeliveryFees = invoice.invoiceData.deliveryFees.length > 0;
+    // if (hasDeliveryFees) {
+    //   InvoiceGenerator.generateAdditionalItemsTable(
+    //     output,
+    //     'Ongkir',
+    //     invoice.invoiceData.deliveryFees,
+    //     priceX,
+    //     subtotalColumnWidth,
+    //     amountX
+    //   );
+    // }
+    // // Additional fees
+    // const hasAdditionalFees = invoice.invoiceData.additionalFees.length > 0;
+    // if (hasAdditionalFees) {
+    //   InvoiceGenerator.generateAdditionalItemsTable(
+    //     output,
+    //     'Penambahan',
+    //     invoice.invoiceData.additionalFees,
+    //     priceX,
+    //     subtotalColumnWidth,
+    //     amountX
+    //   );
+    // }
+    // // Discounts
+    // const hasDiscounts = invoice.invoiceData.discounts.length > 0;
+    // if (hasDiscounts) {
+    //   InvoiceGenerator.generateAdditionalItemsTable(
+    //     output,
+    //     'Pengurangan',
+    //     invoice.invoiceData.discounts,
+    //     priceX,
+    //     subtotalColumnWidth,
+    //     amountX
+    //   );
+    // }
+    // // Grand total
+    // const grandTotal = invoice.getGrandTotal();
+    // InvoiceGenerator.generateGrandTotal(
+    //   output,
+    //   grandTotal,
+    //   'Grand Total:',
+    //   priceX,
+    //   subtotalColumnWidth
+    // );
+    // output.end();
+    // return null;
   }
 }
 
